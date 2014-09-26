6dbd38d9d55338b83a0f18a7aa70f19ef6f04464
rockstarinator <robertjacobswebber@gmail.com>
2014-09-26 01:00:51 -0500

    Updated everything

--- ui.R
+++ ui.R
@@ -1,33 +1,24 @@
 require(shiny); require(rCharts)
 
 shinyUI(pageWithSidebar(
-        headerPanel("CO2 Emissions App"),
+        headerPanel(""),
         sidebarPanel(
-                p('The CO2 emissions app allows you to explore CO2 emissions between 1951 and 2011. 
-                  It is intended as an educational tool that gives accurate scientific information.'),
-                p('Push the slider to explore a different year.
-                  Click or point at a bubble to see what country is represented.
-                  Check a box down below to explore a hypothetical world, where people emit more or less CO2.'),
-                p('FYI, 1 tonne means 1000kg and GDP is measured in 2005 US dollars.'),
-                p('---'),
-                p('To see the R code, go to',
-                  a("github.com/rockstarinator/emissions.", 
-                    href = "https://github.com/rockstarinator/emissions")),
-                p('All data comes from', 
-                  a("Gapminder.org.", href = "http://www.gapminder.org"))
-        ),
-        mainPanel(
+                p(tags$strong("This CO2 emissions enables you to point, click and explore with interactive graphing.")),
+                p(tags$span(style="color:green", "Click or point at a bubble to see what country is represented.")),
                 sliderInput(inputId = "year",
-                            label = "Select a year",
+                            label = tags$span(style="color:red", "Push the slider to explore a different year:"),
                             min = 1951,
                             max = 2011,
-                            value = 2000,
+                            value = 2011,
                             step = 1,
                             format = "####"),
-                showOutput("yearPlot", "highcharts"),
-                checkboxInput(inputId = "special", 
-                              label = "What if all people emitted as much CO2 as Americans?"),
-                checkboxInput(inputId = "special2",
-                              label = "What if all people emitted as little CO2 as the French?")
-        )
+                tags$br(),
+                radioButtons("special", 
+                             label = tags$span(style="color:blue", "Click below to explore a hypothetical world where people emit more or less CO2:"),
+                             choices = list("View real world data." = 1, 
+                                            "What if all people emitted as much CO2 as Americans?" = 2,
+                                            "What if all people emitted as little CO2 as the French?" = 3)
+                             )
+        ), 
+        mainPanel(showOutput("yearPlot", "highcharts"))
 ))
